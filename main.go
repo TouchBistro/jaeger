31fa10ee87fc759fffefde8d127a10a21bba9a71
damianpursey <dpursey@touchbistro.com>
2019-06-19 16:05:27 -0400

    Block organization/comments

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/aws/aws-sdk-go/service/ecs"
@@ -14,44 +13,10 @@ import (
 	"syscall"
 )
 
-func handleAwsErr(err error) {
-	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
-			case ecs.ErrCodeServerException:
-				log.Println(ecs.ErrCodeServerException, aerr.Error())
-			case ecs.ErrCodeClientException:
-				log.Println(ecs.ErrCodeClientException, aerr.Error())
-			case ecs.ErrCodeInvalidParameterException:
-				log.Println(ecs.ErrCodeInvalidParameterException, aerr.Error())
-			case ecs.ErrCodeClusterNotFoundException:
-				log.Println(ecs.ErrCodeClusterNotFoundException, aerr.Error())
-			case ecs.ErrCodeServiceNotFoundException:
-				log.Println(ecs.ErrCodeServiceNotFoundException, aerr.Error())
-			case ecs.ErrCodeServiceNotActiveException:
-				log.Println(ecs.ErrCodeServiceNotActiveException, aerr.Error())
-			case ecs.ErrCodePlatformUnknownException:
-				log.Println(ecs.ErrCodePlatformUnknownException, aerr.Error())
-			case ecs.ErrCodePlatformTaskDefinitionIncompatibilityException:
-				log.Println(ecs.ErrCodePlatformTaskDefinitionIncompatibilityException, aerr.Error())
-			case ecs.ErrCodeAccessDeniedException:
-				log.Println(ecs.ErrCodeAccessDeniedException, aerr.Error())
-			default:
-				log.Println(aerr.Error())
-			}
-		} else {
-			// Print the error, cast err to awserr.Error to get the Code and
-			// Message from an error.
-			log.Println(err.Error())
-		}
-		return
-	}
-}
-
 func main() {
+	//Process flag (Maybe might make this a single arg)
 	service := flag.String("service", "", "Service on ECS to hunt for a container of")
 	flag.Parse()
-
 	if *service == "" {
 		log.Fatal("Unset flags, need -service")
 	}
@@ -63,13 +28,14 @@ func main() {
 	svcEcs := ecs.New(sess)
 
 	listInput := &ecs.ListClustersInput{}
-
 	listResults, err := svcEcs.ListClusters(listInput)
-	handleAwsErr(err)
+	if err != nil {
+		log.Panic(err)
+	}
 
+	//Find which cluster our service is on and some currently running task IDs
 	var clusterArn *string
 	var taskArns []*string
-
 	for _, arn := range listResults.ClusterArns {
 		input := &ecs.ListTasksInput{
 			Cluster:     arn,
@@ -82,20 +48,22 @@ func main() {
 			clusterArn = arn
 		}
 	}
-	if len(taskArns) > 0 {
-		//log.Println(*taskArns[0])
-	} else {
+	if len(taskArns) < 1 {
 		log.Fatal("Could not find service")
 	}
 
+	//Find a container instance ARN from the first running task we found
 	taskInput := &ecs.DescribeTasksInput{
 		Tasks:   taskArns,
 		Cluster: clusterArn,
 	}
 	taskResults, err := svcEcs.DescribeTasks(taskInput)
-	//log.Println(taskResults)
-	handleAwsErr(err)
+	if err != nil {
+		log.Panic(err)
+	}
 	containerArn := taskResults.Tasks[0].ContainerInstanceArn
+
+	//Find the EC2 instance ID of the instance our container is running on
 	containerInput := &ecs.DescribeContainerInstancesInput{
 		ContainerInstances: []*string{containerArn},
 		Cluster:            clusterArn,
@@ -103,21 +71,21 @@ func main() {
 	containerResults, err := svcEcs.DescribeContainerInstances(containerInput)
 	instanceId := containerResults.ContainerInstances[0].Ec2InstanceId
 
+	//Find the private IP of our instance
 	svcEc2 := ec2.New(sess)
 	instanceInput := &ec2.DescribeInstancesInput{
 		InstanceIds: []*string{instanceId},
 	}
 	instanceResults, err := svcEc2.DescribeInstances(instanceInput)
-	//log.Println(instanceResults)
-	handleAwsErr(err)
-
+	if err != nil {
+		log.Panic(err)
+	}
 	instanceIp := instanceResults.Reservations[0].Instances[0].NetworkInterfaces[0].PrivateIpAddress
-	//log.Println(*instanceIp)
 
+	//SSH to the EC2 instance to `docker ps` and find our container id
 	sshString := string("ec2-user@" + *instanceIp)
-	grepString := "docker ps | grep " + *service + " | tr '\\t' ' ' | cut -d ' ' -f 1"
+	grepString := "docker ps | grep " + *service + " | head -n1 | tr '\\t' ' ' | cut -d ' ' -f 1"
 	containerIdCmd := exec.Command("ssh", sshString, grepString)
-
 	containerIdResult, err := containerIdCmd.Output()
 	if err != nil {
 		log.Panic(err)
@@ -125,19 +93,15 @@ func main() {
 	containerId := string(containerIdResult)
 	containerId = strings.TrimSuffix(containerId, "\n")
 
-	//log.Println(containerId)
-
+	//Exec into ssh, passing the container id to drop right into shell
 	runCmd := string("docker exec -ti " + containerId + " sh")
-
 	sshPath, lookErr := exec.LookPath("ssh")
 	if lookErr != nil {
 		log.Panic(lookErr)
 	}
-
 	sshArgs := []string{"ssh", "-t", sshString, runCmd}
-
 	env := os.Environ()
-
+	log.Println(sshArgs)
 	execErr := syscall.Exec(sshPath, sshArgs, env)
 	if execErr != nil {
 		log.Panic(execErr)
