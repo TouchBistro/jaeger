e61369827d222a7e1de1534a58f387bbc7a3d4dd
Christopher Szatmary <cs@christopherszatmary.com>
2020-11-27 20:20:26 -0500

    feat: redesign CLI (#2)
    * feat: complete redesign and rewrite
    
    * Update repo to 2020
    
    * Fix lint error

---
+++ cmd/logs.go
@@ -0,0 +1,52 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/TouchBistro/goutils/fatal"
+	"github.com/TouchBistro/jaeger/aws"
+	"github.com/spf13/cobra"
+)
+
+type logsOptions struct {
+	clusterName      string
+	sshPublicKeyPath string
+}
+
+var logsOpts logsOptions
+
+var logsCmd = &cobra.Command{
+	Use:   "logs <service>",
+	Args:  cobra.ExactArgs(1),
+	Short: "Retrieve logs from dead containers of an ECS service",
+	Run: func(cmd *cobra.Command, args []string) {
+		serviceName := args[0]
+		container, err := aws.FindServiceContainer(aws.FindServiceContainerOptions{
+			ServiceName:      serviceName,
+			ClusterName:      logsOpts.clusterName,
+			SSHPublicKeyPath: logsOpts.sshPublicKeyPath,
+			FindLogs:         true,
+		})
+		if err != nil {
+			fatal.ExitErrf(err, "failed to find container for %s", serviceName)
+		}
+
+		// Exec into ssh, passing the container id to drop right into shell
+		dockerCmd := fmt.Sprintf("docker logs %s", container.ContainerID)
+		execCmd := exec.Command("ssh", "ec2-user@"+container.InstanceDNSName, dockerCmd)
+		execCmd.Stdout = os.Stdout
+		execCmd.Stderr = os.Stderr
+		err = execCmd.Run()
+		if err != nil {
+			fatal.ExitErr(err, "failed to ssh into container")
+		}
+	},
+}
+
+func init() {
+	logsCmd.Flags().StringVar(&logsOpts.clusterName, "cluster", "", "The ECS cluster the service is in")
+	logsCmd.Flags().StringVar(&logsOpts.sshPublicKeyPath, "ssh-key-path", "", "The path to the ssh public key file")
+	rootCmd.AddCommand(logsCmd)
+}
---
+++ cmd/ssh.go
@@ -0,0 +1,53 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/TouchBistro/goutils/fatal"
+	"github.com/TouchBistro/jaeger/aws"
+	"github.com/spf13/cobra"
+)
+
+type sshOptions struct {
+	clusterName      string
+	sshPublicKeyPath string
+}
+
+var sshOpts sshOptions
+
+var sshCmd = &cobra.Command{
+	Use:   "ssh <service>",
+	Args:  cobra.ExactArgs(1),
+	Short: "ssh directly into an ECS container.",
+	Run: func(cmd *cobra.Command, args []string) {
+		serviceName := args[0]
+		container, err := aws.FindServiceContainer(aws.FindServiceContainerOptions{
+			ServiceName:      serviceName,
+			ClusterName:      sshOpts.clusterName,
+			SSHPublicKeyPath: sshOpts.sshPublicKeyPath,
+		})
+		if err != nil {
+			fatal.ExitErrf(err, "failed to find container for %s", serviceName)
+		}
+
+		// Exec into ssh, passing the container id to drop right into shell
+		dockerCmd := fmt.Sprintf("docker exec -it %s bash", container.ContainerID)
+		execCmd := exec.Command("ssh", "-t", "ec2-user@"+container.InstanceDNSName, dockerCmd)
+		execCmd.Stdin = os.Stdin
+		execCmd.Stdout = os.Stdout
+		execCmd.Stderr = os.Stderr
+
+		err = execCmd.Run()
+		if err != nil {
+			fatal.ExitErr(err, "failed to ssh into container")
+		}
+	},
+}
+
+func init() {
+	sshCmd.Flags().StringVar(&sshOpts.clusterName, "cluster", "", "The ECS cluster the service is in")
+	sshCmd.Flags().StringVar(&sshOpts.sshPublicKeyPath, "ssh-key-path", "", "The path to the ssh public key file")
+	rootCmd.AddCommand(sshCmd)
+}
