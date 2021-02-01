bacba15c368132c316b90753a8c24f920701c6fd
Christopher Szatmary <cs@christopherszatmary.com>
2021-02-01 17:40:05 -0500

    chore: Use aws sdk v2

--- aws/aws.go
+++ aws/aws.go
@@ -1,60 +1,60 @@
 package aws
 
 import (
+	"context"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/ec2"
-	"github.com/aws/aws-sdk-go/service/ec2instanceconnect"
-	"github.com/aws/aws-sdk-go/service/ecs"
+	"github.com/TouchBistro/goutils/fatal"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2instanceconnect"
+	"github.com/aws/aws-sdk-go-v2/service/ecs"
+	ecstypes "github.com/aws/aws-sdk-go-v2/service/ecs/types"
 	"github.com/pkg/errors"
 )
 
-var ecsClient *ecs.ECS
-var ec2ICClient *ec2instanceconnect.EC2InstanceConnect
-var ec2Client *ec2.EC2
+var awsConfig aws.Config
 
 func init() {
-	sess := session.Must(session.NewSessionWithOptions(session.Options{
-		// Load the user's config from the env
-		// This way it will automatically use the profile the user has configured
-		SharedConfigState: session.SharedConfigEnable,
-	}))
-	ecsClient = ecs.New(sess)
-	ec2ICClient = ec2instanceconnect.New(sess)
-	ec2Client = ec2.New(sess)
+	var err error
+	awsConfig, err = config.LoadDefaultConfig(context.Background())
+	if err != nil {
+		fatal.ExitErr(err, "Failed to initialize AWS config")
+	}
 }
 
 // ListServices returns the names of all available ECS services.
 func ListServices() ([]string, error) {
+	ecsClient := ecs.NewFromConfig(awsConfig)
+	ctx := context.Background()
+
 	// This is paginated but if we ever have that many clusters that
 	// it's an issue then we have a bigger problem
-	respListClusters, err := ecsClient.ListClusters(&ecs.ListClustersInput{
-		MaxResults: aws.Int64(100),
+	listClustersOutput, err := ecsClient.ListClusters(ctx, &ecs.ListClustersInput{
+		MaxResults: aws.Int32(100),
 	})
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to list available ECS Clusters")
 	}
 
 	var serviceNames []string
-	for _, clusterARN := range respListClusters.ClusterArns {
-		respListServices, err := ecsClient.ListServices(&ecs.ListServicesInput{
-			Cluster:    clusterARN,
-			MaxResults: aws.Int64(100),
+	for _, clusterARN := range listClustersOutput.ClusterArns {
+		listServicesOutput, err := ecsClient.ListServices(ctx, &ecs.ListServicesInput{
+			Cluster:    aws.String(clusterARN),
+			MaxResults: aws.Int32(100),
 		})
 		if err != nil {
-			return nil, errors.Wrapf(err, "failed to list services in cluster %s", *clusterARN)
+			return nil, errors.Wrapf(err, "failed to list services in cluster %s", clusterARN)
 		}
 
-		for _, arn := range respListServices.ServiceArns {
+		for _, arn := range listServicesOutput.ServiceArns {
 			// TODO(@cszatmary): Use a regex to parse the ARN
-			parts := strings.Split(*arn, "/")
+			parts := strings.Split(arn, "/")
 			serviceNames = append(serviceNames, parts[len(parts)-1])
 		}
 	}
@@ -75,59 +75,60 @@ type ServiceContainer struct {
 }
 
 func FindServiceContainer(opts FindServiceContainerOptions) (ServiceContainer, error) {
-	var clusterARNs []*string
+	ecsClient := ecs.NewFromConfig(awsConfig)
+	ctx := context.Background()
+	var clusterARNs []string
 	if opts.ClusterName != "" {
-		respDescribeClusters, err := ecsClient.DescribeClusters(&ecs.DescribeClustersInput{
-			Clusters: []*string{aws.String(opts.ClusterName)},
+		describeClustersOutput, err := ecsClient.DescribeClusters(ctx, &ecs.DescribeClustersInput{
+			Clusters: []string{opts.ClusterName},
 		})
 		if err != nil {
 			return ServiceContainer{}, errors.Wrapf(err, "failed to find cluster %s", opts.ClusterName)
 		}
 
-		if len(respDescribeClusters.Clusters) == 0 {
+		if len(describeClustersOutput.Clusters) == 0 {
 			return ServiceContainer{}, errors.Errorf("no cluster named %s found", opts.ClusterName)
 		}
 
-		clusterARNs = []*string{respDescribeClusters.Clusters[0].ClusterArn}
+		clusterARNs = []string{*describeClustersOutput.Clusters[0].ClusterArn}
 	} else {
-		respListClusters, err := ecsClient.ListClusters(&ecs.ListClustersInput{
-			MaxResults: aws.Int64(100),
+		listClustersOutput, err := ecsClient.ListClusters(ctx, &ecs.ListClustersInput{
+			MaxResults: aws.Int32(100),
 		})
 		if err != nil {
 			return ServiceContainer{}, errors.Wrap(err, "failed to list available ECS clusters")
 		}
 
-		clusterARNs = respListClusters.ClusterArns
+		clusterARNs = listClustersOutput.ClusterArns
 	}
 
 	// Find which cluster our service is on and some currently running task IDs
-	desiredStatus := "RUNNING"
+	desiredStatus := ecstypes.DesiredStatusRunning
 	if opts.FindLogs {
-		desiredStatus = "STOPPED"
+		desiredStatus = ecstypes.DesiredStatusStopped
 	}
 
-	var clusterARN *string
-	var taskArns []*string
+	var clusterARN string
+	var taskArns []string
 	for _, arn := range clusterARNs {
-		respListTasks, err := ecsClient.ListTasks(&ecs.ListTasksInput{
-			Cluster:       arn,
+		listTasksOutput, err := ecsClient.ListTasks(ctx, &ecs.ListTasksInput{
+			Cluster:       aws.String(arn),
 			ServiceName:   aws.String(opts.ServiceName),
-			DesiredStatus: aws.String(desiredStatus),
-			MaxResults:    aws.Int64(100),
+			DesiredStatus: desiredStatus,
+			MaxResults:    aws.Int32(100),
 		})
 		if err != nil {
 			// Ignore if not found since we are checking multiple clusters
-			aerr, ok := err.(awserr.Error)
-			if ok && aerr.Code() == ecs.ErrCodeServiceNotFoundException {
+			var notFoundErr *ecstypes.ResourceNotFoundException
+			if errors.As(err, &notFoundErr) {
 				continue
 			}
-
-			return ServiceContainer{}, errors.Wrapf(err, "failed to list tasks for %s in cluster %s", opts.ServiceName, *arn)
+			return ServiceContainer{}, errors.Wrapf(err, "failed to list tasks for %s in cluster %s", opts.ServiceName, arn)
 		}
 
 		// Stop after finding a match
 		clusterARN = arn
-		taskArns = respListTasks.TaskArns
+		taskArns = listTasksOutput.TaskArns
 		break
 	}
 
@@ -140,8 +141,8 @@ func FindServiceContainer(opts FindServiceContainerOptions) (ServiceContainer, e
 	}
 
 	// Find a container instance ARN from the first running task we found
-	respDescribeTasks, err := ecsClient.DescribeTasks(&ecs.DescribeTasksInput{
-		Cluster: clusterARN,
+	describeTasksOutput, err := ecsClient.DescribeTasks(ctx, &ecs.DescribeTasksInput{
+		Cluster: aws.String(clusterARN),
 		Tasks:   taskArns,
 	})
 	if err != nil {
@@ -149,7 +150,7 @@ func FindServiceContainer(opts FindServiceContainerOptions) (ServiceContainer, e
 	}
 
 	// Just use the first one
-	task := respDescribeTasks.Tasks[0]
+	task := describeTasksOutput.Tasks[0]
 	// TODO(@cszatmary): Use a regex to parse the ARN
 	parts := strings.Split(*task.TaskDefinitionArn, "/")
 	taskDefName := parts[len(parts)-1]
@@ -157,22 +158,23 @@ func FindServiceContainer(opts FindServiceContainerOptions) (ServiceContainer, e
 	taskDefName = strings.Replace(taskDefName, ":", "-", -1)
 
 	// Find the EC2 instance ID of the instance our container is running on
-	respDescribeContainerInstances, err := ecsClient.DescribeContainerInstances(&ecs.DescribeContainerInstancesInput{
-		Cluster:            clusterARN,
-		ContainerInstances: []*string{task.ContainerInstanceArn},
+	describeContainerInstancesOutput, err := ecsClient.DescribeContainerInstances(ctx, &ecs.DescribeContainerInstancesInput{
+		Cluster:            aws.String(clusterARN),
+		ContainerInstances: []string{*task.ContainerInstanceArn},
 	})
 	if err != nil {
 		return ServiceContainer{}, errors.Wrapf(err, "failed to describe container instance for %s", opts.ServiceName)
 	}
 
-	if len(respDescribeContainerInstances.ContainerInstances) == 0 {
+	if len(describeContainerInstancesOutput.ContainerInstances) == 0 {
 		return ServiceContainer{}, errors.Errorf("no container instances found for %s", opts.ServiceName)
 	}
 
 	// Find the private IP of our instance
-	instanceID := respDescribeContainerInstances.ContainerInstances[0].Ec2InstanceId
-	respDescribeInstances, err := ec2Client.DescribeInstances(&ec2.DescribeInstancesInput{
-		InstanceIds: []*string{instanceID},
+	instanceID := describeContainerInstancesOutput.ContainerInstances[0].Ec2InstanceId
+	ec2Client := ec2.NewFromConfig(awsConfig)
+	describeInstancesOutput, err := ec2Client.DescribeInstances(ctx, &ec2.DescribeInstancesInput{
+		InstanceIds: []string{*instanceID},
 	})
 	if err != nil {
 		return ServiceContainer{}, errors.Wrapf(err, "failed to find EC2 instance for %s", opts.ServiceName)
@@ -201,8 +203,9 @@ func FindServiceContainer(opts FindServiceContainerOptions) (ServiceContainer, e
 	}
 
 	// TODO(@cszatmary): This is kinda sketchy. This is a great place for an out of bounds panic
-	instance := respDescribeInstances.Reservations[0].Instances[0]
-	respSendSSHPublicKey, err := ec2ICClient.SendSSHPublicKey(&ec2instanceconnect.SendSSHPublicKeyInput{
+	instance := describeInstancesOutput.Reservations[0].Instances[0]
+	ec2ICClient := ec2instanceconnect.NewFromConfig(awsConfig)
+	sendSSHPublicKeyOutput, err := ec2ICClient.SendSSHPublicKey(ctx, &ec2instanceconnect.SendSSHPublicKeyInput{
 		AvailabilityZone: instance.Placement.AvailabilityZone,
 		InstanceId:       instanceID,
 		InstanceOSUser:   aws.String("ec2-user"),
@@ -211,9 +214,7 @@ func FindServiceContainer(opts FindServiceContainerOptions) (ServiceContainer, e
 	if err != nil {
 		return ServiceContainer{}, errors.Wrapf(err, "failed to send SSH key to EC2 instance %s", *instanceID)
 	}
-
-	// Triple state failures are the best
-	if respSendSSHPublicKey.Success == nil || !*respSendSSHPublicKey.Success {
+	if !sendSSHPublicKeyOutput.Success {
 		return ServiceContainer{}, errors.Errorf("failed to send ssh key to EC2 instance %s", *instanceID)
 	}
 
